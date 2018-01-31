927bd20b5105cb3d0789b13519aa8be93b872b9e
Neil McEachin <njmceach@mtu.edu>
2018-01-31 17:04:08 -0500

    Added a add item function and double click to delete item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,24 @@ import { AppComponent } from './app.component';
 import { ItemsComponent } from './components/items/items.component';
 
 import { ItemService } from './services/item.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
+
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemsComponent,
+    NavbarComponent,
+    AddItemComponent,
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase, 'angularfs'),
     AngularFirestoreModule,
+    FormsModule
   ],
   providers: [ItemService],
   bootstrap: [AppComponent]
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -7,13 +7,31 @@ import { Observable } from 'rxjs/Observable';
 export class ItemService {
   itemsCollection: AngularFirestoreCollection<Item>;
   items: Observable<Item[]>;
+  itemDoc: AngularFirestoreDocument<Item>;
 
   constructor(public afs: AngularFirestore) { 
-    this.items = this.afs.collection('items').valueChanges();
+    //this.items = this.afs.collection('items').valueChanges();
+    this.itemsCollection = this.afs.collection('items', ref => ref.orderBy('title','asc'));
+    this.items = this.itemsCollection.snapshotChanges().map(changes => {
+      return changes.map(a => {
+        const data = a.payload.doc.data() as Item;
+        data.id = a.payload.doc.id;
+        return data;
+      });
+    });
   }
 
   getItems(){
     return this.items;
+  }
+  
+  addItem(item){
+    this.itemsCollection.add(item);
+  }
+
+  deleteItem(item: Item){
+    this.itemDoc = this.itemsCollection.doc(`${item.id}`);
+    this.itemDoc.delete();
   }
 
 }
